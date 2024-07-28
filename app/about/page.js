1ada5552e28dd1d88b4972a9ad2e25a8643656c5
kangzBredz04 <wabredz1234@gmail.com>
2024-07-28 11:23:29 +0700

    update tampilan halaman about

--- app/about/page.js
+++
@@ -1,7 +0,0 @@
-export default function Page() {
-  return (
-    <main>
-      <h1>Tentang Getch Foods</h1>
-    </main>
-  );
-}
---
+++ app/about/page.tsx
@@ -0,0 +1,11 @@
+import Button from "@/components/button";
+
+export default function Page() {
+  return (
+    <main>
+      <h1>Tentang Getch Foods</h1>
+      <p>Getch Foods adalah...</p>
+      <Button href={"/"} text={"Kembali ke beranda"} />
+    </main>
+  );
+}
